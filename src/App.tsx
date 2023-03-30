a218c94b23ec734af5bf5a6fa33959a41a3d091b
Lurang <lurangjh@naver.com>
2023-03-30 14:09:05 +0900

    default logo 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const checkDev = () => process.env.NODE_ENV === 'production'
+    ? <></>
+    : <>Dev Mode Detected</>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body className="App-body">
+        <h1>Hello GDSC</h1>
+        {checkDev()}
+      </body>
     </div>
   );
 }
