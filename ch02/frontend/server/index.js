cca5696a073b77d6332bfc1f7ac83185682616b7
Lurang <lurangjh@naver.com>
2023-04-05 17:09:56 +0900

    ch02 수정

--- ch02/frontend/server/index.js
+++ ch02/frontend/server/index.js
@@ -2,11 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT=4000;
+const PORT = 4000;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', (_, res) => {
     res.send(path.join(__dirname, '../build/index.html'));
 });
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`[Info] Server is listening on port ${PORT}`);
+});
